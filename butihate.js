d5e139e09fb8a3e9dc91092f1d3b9935f0705c47
ivysat <31290932+ivysat@users.noreply.github.com>
2018-12-11 01:42:01 -0500

    Update butihate.js

--- butihate.js
+++ butihate.js
@@ -81,43 +81,34 @@ var build_first_interface = function () {
 	}
 	
 	function autocomplete(inp, arr) {
-		/*the autocomplete function takes two arguments,
-		the text field element and an array of possible autocompleted values:*/
-
 		/*execute a function when someone writes in the text field:*/
 		inp.addEventListener("input", function(e) {
-			var a, b, i, val = this.value;
+			var autoList, autoElt, i, val = this.value;
 			/*close any already open lists of autocompleted values*/
 			closeAllLists();
 			if (!val) { return false;}
 			/*create a DIV element that will contain the items (values):*/
-			a = document.createElement("DIV");
-			a.setAttribute("id", this.id + "autocomplete-list");
-			a.setAttribute("class", "autocomplete-items");
-			/*append the DIV element as a child of the autocomplete container:*/
-			this.parentNode.appendChild(a);
-			/*for each item in the array...*/
+			autoList = document.createElement("DIV");
+			autoList.setAttribute("id", this.id + "autocomplete-list");
+			autoList.setAttribute("class", "autocomplete-items");
+			this.parentNode.appendChild(autoList);
+
 			for (i = 0; i < arr.length; i++) {
-			  /*check if the item starts with the same letters as the text field value:*/
 			  if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
 				/*create a DIV element for each matching element:*/
-				b = document.createElement("DIV");
-				/*make the matching letters bold:*/
-				b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-				b.innerHTML += arr[i].substr(val.length);
+				autoElt = document.createElement("DIV");
+				autoElt.append(arr[i]);
 				/*insert a input field that will hold the current array item's value:*/
-				b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-				/*execute a function when someone clicks on the item value (DIV element):*/
-					b.addEventListener("click", function(e) {
+				autoElt.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+				autoElt.addEventListener("click", function(e) {
 					/*insert the value for the autocomplete text field:*/
 					inp.value = this.getElementsByTagName("input")[0].value;
-					/*close the list of autocompleted values,
-					(or any other open lists of autocompleted values:*/
 					closeAllLists();
 				});
-				a.appendChild(b);
+				autoList.appendChild(autoElt);
 			  }
 			}
+
 		});
 
 		function closeAllLists(elmnt) {
