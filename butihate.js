012f65634a47ec01445d23f0f3bef0099b7ad514
Parker Loflin <parkerbrayden@gmail.com>
2018-12-10 22:16:12 -0500

    Update butihate.js

--- butihate.js
+++ butihate.js
@@ -1,6 +1,40 @@
 root_url = "http://comp426.cs.unc.edu:3001";
 
-var build_question_interface = function () {
+
+$(document).ready(() => {
+    $('#login_btn').on('click', () => {
+	
+	let user = $('#login_user').val();
+	let pass = $('#login_pass').val();
+
+	console.log(user);
+	console.log(pass);
+	
+	build_first_interface();
+
+	// $.ajax(root_url + 'login',
+	//        {
+	// 	   type: 'GET',
+	// 	   xhrFields: {withCredentials: true},
+	// 	   data: {
+	// 	       username: user,
+	// 	       password: pass
+	// 	   },
+	// 	   success: (response) => {
+	// 	       if (response.status) {
+	// 		   build_question_interface();
+	// 	       } else {
+	// 		   $('#mesg_div').html("Login failed. Try again.");
+	// 	       }
+	// 	   },
+	// 	   error: () => {
+	// 	       alert('error');
+	// 	   }
+	//        });
+    // });
+});
+
+var build_first_interface = function () {
     let body = $('body');
 
     body.empty();
