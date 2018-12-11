75912881278bb85a5f2e2eb708ddad584ffec25c
ivysat <31290932+ivysat@users.noreply.github.com>
2018-12-11 01:11:30 -0500

    Update butihate.js

--- butihate.js
+++ butihate.js
@@ -35,7 +35,7 @@ $(document).ready(() => {
 var build_first_interface = function () {
 	let body = $('body');
 	
-	let cities = [0: "Kansas City","Phoenix","Newark","Fort Lauderdale","Miami","Long Beach","Savannah","Dayton","Little Rock","New York","Memphis","Boise","El Paso",
+	let cities = ["Kansas City","Phoenix","Newark","Fort Lauderdale","Miami","Long Beach","Savannah","Dayton","Little Rock","New York","Memphis","Boise","El Paso",
 	"Seattle","Indianapolis","Lihue","San Antonio","Providence","Tampa","Nashville","Portland","Honolulu","Fort Myers","Windsor Locks","Palm Springs","Agana","Oakland",
 	"Kahului","San Francisco","Cleveland","Kona","Washington","Houston","Norfolk","Raleigh-durham","Austin", "Jacksonville", "San Diego","Syracuse","Tulsa","Albany",
 	"Charlotte","Boston", "Wichita","Spokane","Los Angeles","Pensacola", "Pittsburgh","White Plains","St. Petersburg","Tucson","Burbank","Knoxville","Atlanta","Ontario",
@@ -45,7 +45,7 @@ var build_first_interface = function () {
 
     body.empty();
 
-	body.append('<h1 id = "titleHeader">I Wanna Leave <input type = "text" id = "departure" placeholder = "Raleigh" </input>, But I Hate...</h1>');
+	body.append('<h1 id = "titleHeader">I Wanna Leave  <div class="autocomplete" style="width:300px;"><input type = "text" id = "departure" placeholder = "Raleigh" </input></div>, But I Hate...</h1>');
 	body.append("<button id='search_loc'>Let's Go!</button></div>");
 	body.append('<div id = "containerDiv"> <div id = "leftDiv"></div> <div id = "rightDiv"></div> </div>');
 
@@ -75,39 +75,109 @@ var build_first_interface = function () {
     let qlist = $('<div></div>');
 
 
-    // $.ajax(root_url + "/questions",
-	//    {
-	//        type: 'GET',
-	//        dataType: 'json',
-	//        xhrFields: {withCredentials: true},
-	//        success: (response) => {
-	// 	   let qarray = response.data;
-	// 	   for (let i=0; i<qarray.length; i++) {
-	// 	       let qdiv = create_question_div(qarray[i]);
-	// 	       qlist.append(qdiv);
-	// 	       let qid = qarray[i].id
-	// 	       $.ajax(root_url + 'answers/' + qid,
-	// 		      {
-	// 			  type: 'GET',
-	// 			  dataType: 'json',
-	// 			  xhrFields: {withCredentials: true},
-	// 			  success: (response) => {
-	// 			      if (response.data != null) {
-	// 				  let answer = response.data;
-	// 				  qdiv.append('<div class="answer" id="aid_' + answer.answer_id + '">' +
-	// 					      answer.answer_text + '</div>');
-	// 				  qdiv.addClass('answered');
-	// 			      }
-	// 			  }
-	// 		      }); 
-	// 	   }
-	//        }
-	//    });
-
     let create_question_div = (question) => {
 	let qdiv = $('<div class="question" id="qid_' + question.id + '"></div>');
 	qdiv.append('<div class="qtitle">' + question.title + '</div>');
 	qdiv.append('<div class="count">' + question.answerCount + '</div>');
 	return qdiv;
-    }
+	}
+	
+	function autocomplete(inp, arr) {
+		/*the autocomplete function takes two arguments,
+		the text field element and an array of possible autocompleted values:*/
+		var currentFocus;
+		/*execute a function when someone writes in the text field:*/
+		inp.addEventListener("input", function(e) {
+			var a, b, i, val = this.value;
+			/*close any already open lists of autocompleted values*/
+			closeAllLists();
+			if (!val) { return false;}
+			currentFocus = -1;
+			/*create a DIV element that will contain the items (values):*/
+			a = document.createElement("DIV");
+			a.setAttribute("id", this.id + "autocomplete-list");
+			a.setAttribute("class", "autocomplete-items");
+			/*append the DIV element as a child of the autocomplete container:*/
+			this.parentNode.appendChild(a);
+			/*for each item in the array...*/
+			for (i = 0; i < arr.length; i++) {
+			  /*check if the item starts with the same letters as the text field value:*/
+			  if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+				/*create a DIV element for each matching element:*/
+				b = document.createElement("DIV");
+				/*make the matching letters bold:*/
+				b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+				b.innerHTML += arr[i].substr(val.length);
+				/*insert a input field that will hold the current array item's value:*/
+				b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+				/*execute a function when someone clicks on the item value (DIV element):*/
+					b.addEventListener("click", function(e) {
+					/*insert the value for the autocomplete text field:*/
+					inp.value = this.getElementsByTagName("input")[0].value;
+					/*close the list of autocompleted values,
+					(or any other open lists of autocompleted values:*/
+					closeAllLists();
+				});
+				a.appendChild(b);
+			  }
+			}
+		});
+		/*execute a function presses a key on the keyboard:*/
+		inp.addEventListener("keydown", function(e) {
+			var x = document.getElementById(this.id + "autocomplete-list");
+			if (x) x = x.getElementsByTagName("div");
+			if (e.keyCode == 40) {
+			  /*If the arrow DOWN key is pressed,
+			  increase the currentFocus variable:*/
+			  currentFocus++;
+			  /*and and make the current item more visible:*/
+			  addActive(x);
+			} else if (e.keyCode == 38) { //up
+			  /*If the arrow UP key is pressed,
+			  decrease the currentFocus variable:*/
+			  currentFocus--;
+			  /*and and make the current item more visible:*/
+			  addActive(x);
+			} else if (e.keyCode == 13) {
+			  /*If the ENTER key is pressed, prevent the form from being submitted,*/
+			  e.preventDefault();
+			  if (currentFocus > -1) {
+				/*and simulate a click on the "active" item:*/
+				if (x) x[currentFocus].click();
+			  }
+			}
+		});
+		function addActive(x) {
+		  /*a function to classify an item as "active":*/
+		  if (!x) return false;
+		  /*start by removing the "active" class on all items:*/
+		  removeActive(x);
+		  if (currentFocus >= x.length) currentFocus = 0;
+		  if (currentFocus < 0) currentFocus = (x.length - 1);
+		  /*add class "autocomplete-active":*/
+		  x[currentFocus].classList.add("autocomplete-active");
+		}
+		function removeActive(x) {
+		  /*a function to remove the "active" class from all autocomplete items:*/
+		  for (var i = 0; i < x.length; i++) {
+			x[i].classList.remove("autocomplete-active");
+		  }
+		}
+		function closeAllLists(elmnt) {
+		  /*close all autocomplete lists in the document,
+		  except the one passed as an argument:*/
+		  var x = document.getElementsByClassName("autocomplete-items");
+		  for (var i = 0; i < x.length; i++) {
+			if (elmnt != x[i] && elmnt != inp) {
+			x[i].parentNode.removeChild(x[i]);
+		  }
+		}
+	  }
+	  /*execute a function when someone clicks in the document:*/
+	  document.addEventListener("click", function (e) {
+		  closeAllLists(e.target);
+	  });
+	  }
+
+	  autocomplete(document.getElementById("departure"), cities);
 };
