831cdc6114a668e926704043e5c6134ea4122f12
ivysat <31290932+ivysat@users.noreply.github.com>
2018-12-11 01:23:51 -0500

    less fluff

--- butihate.js
+++ butihate.js
@@ -83,14 +83,13 @@ var build_first_interface = function () {
 	function autocomplete(inp, arr) {
 		/*the autocomplete function takes two arguments,
 		the text field element and an array of possible autocompleted values:*/
-		var currentFocus;
+
 		/*execute a function when someone writes in the text field:*/
 		inp.addEventListener("input", function(e) {
 			var a, b, i, val = this.value;
 			/*close any already open lists of autocompleted values*/
 			closeAllLists();
 			if (!val) { return false;}
-			currentFocus = -1;
 			/*create a DIV element that will contain the items (values):*/
 			a = document.createElement("DIV");
 			a.setAttribute("id", this.id + "autocomplete-list");
@@ -120,47 +119,7 @@ var build_first_interface = function () {
 			  }
 			}
 		});
-		/*execute a function presses a key on the keyboard:*/
-		inp.addEventListener("keydown", function(e) {
-			var x = document.getElementById(this.id + "autocomplete-list");
-			if (x) x = x.getElementsByTagName("div");
-			if (e.keyCode == 40) {
-			  /*If the arrow DOWN key is pressed,
-			  increase the currentFocus variable:*/
-			  currentFocus++;
-			  /*and and make the current item more visible:*/
-			  addActive(x);
-			} else if (e.keyCode == 38) { //up
-			  /*If the arrow UP key is pressed,
-			  decrease the currentFocus variable:*/
-			  currentFocus--;
-			  /*and and make the current item more visible:*/
-			  addActive(x);
-			} else if (e.keyCode == 13) {
-			  /*If the ENTER key is pressed, prevent the form from being submitted,*/
-			  e.preventDefault();
-			  if (currentFocus > -1) {
-				/*and simulate a click on the "active" item:*/
-				if (x) x[currentFocus].click();
-			  }
-			}
-		});
-		function addActive(x) {
-		  /*a function to classify an item as "active":*/
-		  if (!x) return false;
-		  /*start by removing the "active" class on all items:*/
-		  removeActive(x);
-		  if (currentFocus >= x.length) currentFocus = 0;
-		  if (currentFocus < 0) currentFocus = (x.length - 1);
-		  /*add class "autocomplete-active":*/
-		  x[currentFocus].classList.add("autocomplete-active");
-		}
-		function removeActive(x) {
-		  /*a function to remove the "active" class from all autocomplete items:*/
-		  for (var i = 0; i < x.length; i++) {
-			x[i].classList.remove("autocomplete-active");
-		  }
-		}
+
 		function closeAllLists(elmnt) {
 		  /*close all autocomplete lists in the document,
 		  except the one passed as an argument:*/
