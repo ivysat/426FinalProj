036e2dfecc3be83f1f0ad8e511688ea0fcb9b645
Parker Loflin <parkerbrayden@gmail.com>
2018-12-14 02:26:04 -0500

    lollipop

--- butihate.js
+++ butihate.js
@@ -46,7 +46,7 @@ var build_first_interface = function () {
     body.empty();
 
 	body.append('<h1 id = "titleHeader">I Hate  <div class="autocomplete"><input type = "text" id = "departure" placeholder = "Raleigh-durham" </input></div>, But I <i>Really</i> Hate...</h1>');
-	body.append('<div id = "containerDiv"> <div id = "leftDiv"><div id="whatTheyHateTitleContainer"><h2 id = "whatTheyHateTitle">What do you really hate?</h2><div id = "radioButtonContainer"></div></div></div> <div id = "rightDiv" class="rhsDiv"><div id="flightDataTitleContainer"><h2 id = "flightDataTitle">Sheesh, maybe you won\'t hate these flights...</h2></div></div> </div>');
+	body.append('<div id = "containerDiv"> <div id = "leftDiv"><div id="whatTheyHateTitleContainer"><h2 id = "whatTheyHateTitle">What do you really hate?</h2><div id = "radioButtonContainer"></div></div></div> <div id = "rightDiv" class="rhsDiv"><div id="flightDataTitleContainer"><h2 id = "flightDataTitle">Sheesh, you can check the counts on these flights...</h2></div></div> </div>');
 	let radioButtonContainerDiv = $('#radioButtonContainer').append('<input type="radio" name="age" value="babies"> Babies<br><input type="radio" name="age" value="children"> Children<br><input type="radio" name="age" value="Teenagers"> Teenagers<br><input type="radio" name="age" value="millenials"> Millenials<br><input type="radio" name="age" value="genXers"> Gen Xers<br><input type="radio" name="age" value="boomers"> Baby Boomers<br><input type="radio" name="age" value="traditionalists"> Traditionalists<br>');
 
     $('input[type="radio"]').on('click', () => {
@@ -319,6 +319,8 @@ var build_second_interface = function(){
 	console.log("clearing");
 	let outerContainer = $("#containerDiv");
 	outerContainer.empty();
-	outerContainer.html('<div id = bookFlightContainer><div id="dataInputContainer"><input type = "text" class="bookData" id = "firstName" placeholder = "First name"</input><input type = "text" class="bookData" id = "lastName" placeholder = "Last name"</input><input type = "text" class="bookData" id = "age" placeholder = "Age (1+)" </input><input type = "text" class="bookData" id = "gender" placeholder = "Gender"</input></div><button type = "button" id = "goBackButton" onclick = "$("body").empty();build_first_interface()">I Hate This, Go Back!</button></div>');
-
+	outerContainer.html('<div id = bookFlightContainer><div id="dataInputContainer"><input type = "text" class="bookData" id = "firstName" placeholder = "First name"</input><input type = "text" class="bookData" id = "lastName" placeholder = "Last name"</input><input type = "text" class="bookData" id = "age" placeholder = "Age (1+)" </input><input type = "text" class="bookData" id = "gender" placeholder = "Gender"</input></div><button type = "button" id = "goBackButton">I Hate This, Go Back!</button></div>');
+	$( "#goBackButton" ).click(function() {
+		build_first_interface();
+	});
 };
